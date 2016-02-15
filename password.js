76c9f5213a3feefb9c7c841c99f70eaa5b3633fe
Josiah Gray <j305g268@ku.edu>
2016-02-15 15:58:34 -0600

    Add passwordValidator.html and password.js Complete exercise 1

---
+++ password.js
@@ -0,0 +1,20 @@
+function validate()
+{
+  var pass1 = document.getElementById("password1").value;
+  var pass2 = document.getElementById("password2").value;
+  if((pass1.length < 8) || (pass2.length < 8))
+  {
+    alert("Passwords are not at least 8 characters long!");
+  }
+  else
+  {
+    if(pass1 != pass2)
+    {
+      alert("Passwords do not match!");
+    }
+    else
+    {
+      alert("Passwords match!");
+    }
+  }
+}
