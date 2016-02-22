b91f8f2111c981a2497c67d6dc84f686974c4101
Josiah Gray <j305g268@ku.edu>
2016-02-22 09:30:26 -0600

    Add manipulation.html, manipulationCSS.css, and manipulate.js Complete exercise 4

---
+++ manipulate.js
@@ -0,0 +1,106 @@
+function changeBorder()
+{
+	var red = document.getElementById("borderR").value;
+	var green = document.getElementById("borderG").value;
+	var blue = document.getElementById("borderB").value;
+	var par = document.getElementById("myP");
+	
+	/*
+	if(typeof red === "number")
+	{
+		red = parseInt(red);
+		if(red > 255 || red < 0)
+		{
+			red = 0;
+		}
+	}
+	else
+	{
+		red = 0;
+	}
+	
+	if(typeof green === "number")
+	{
+		green = parseInt(green);
+		if(green > 255 || green < 0)
+		{
+			green = 0;
+		}
+	}
+	else
+	{
+		green = 0;
+	}
+	
+	if(typeof blue === "number")
+	{
+		blue = parseInt(blue);
+		if(blue > 255 || blue < 0)
+		{
+			blue = 0;
+		}
+	}
+	else
+	{
+		blue = 0;
+	}
+	
+	par.style.borderColor = "rgb(" + red + ", " + green ", " + blue + ")";
+	*/
+	
+	par.style.borderColor = "#" + red + green + blue;
+}
+
+function changeBackground()
+{
+	var red = document.getElementById("backR").value;
+	var green = document.getElementById("backG").value;
+	var blue = document.getElementById("backB").value;
+	var par = document.getElementById("myP");
+	
+	/*
+	if(typeof red === "number")
+	{
+		red = parseInt(red);
+		if(red > 255 || red < 0)
+		{
+			red = 0;
+		}
+	}
+	else
+	{
+		red = 0;
+	}
+	
+	if(typeof green === "number")
+	{
+		green = parseInt(green);
+		if(green > 255 || green < 0)
+		{
+			green = 0;
+		}
+	}
+	else
+	{
+		green = 0;
+	}
+	
+	if(typeof blue === "number")
+	{
+		blue = parseInt(blue);
+		if(blue > 255 || blue < 0)
+		{
+			blue = 0;
+		}
+	}
+	else
+	{
+		blue = 0;
+	}
+	
+	par.style.backgroundColor = "rgb(" + red + "," + green "," + blue + ")";
+	*/
+	
+	par.style.backgroundColor = "#" + red + green + blue;
+	
+}
