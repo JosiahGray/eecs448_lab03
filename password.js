b8a3b10ecf5eacc95d050b6e194659616afb8982
Josiah Gray <j305g268@ku.edu>
2016-02-22 09:25:15 -0600

    Fix password.js

--- password.js
+++ password.js
@@ -2,6 +2,7 @@ function validate()
 {
   var pass1 = document.getElementById("password1").value;
   var pass2 = document.getElementById("password2").value;
+  
   if((pass1.length < 8) || (pass2.length < 8))
   {
     alert("Passwords are not at least 8 characters long!");
